44ac80244eaca1f8e3c527bb5cf6cd173b8ef83d
yountaeseop <jangmj6931@gmail.com>
2022-11-03 01:32:40 +0900

    게시물 리스트 기능 구현

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,5 +1,7 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,23 +9,35 @@ public class Main {
 	public static void main(String[] args) {
 
 		System.out.println("== 프로그램 시작");
-		int id = 1;
-
+		Scanner sc = new Scanner(System.in);
+		int lastArticleId = 0;
+		
+		List<Article> articles = new ArrayList<>();
+		
 		while (true) {
 			System.out.print("명령어) ");
-			Scanner sc = new Scanner(System.in);
-			String input = sc.nextLine();
+			String input = sc.nextLine().trim();
 
 			if (input.equals("article list")) {
-				System.out.println("게시글이 없습니다.");
+				System.out.println("번호  /  제목");
+				for(int i = 0; i < articles.size(); i++) {
+					System.out.println(articles.get(i));
+				}
+				
 			} else if (input.equals("article write")) {
+				
+				int id = lastArticleId + 1;
+				lastArticleId = id;
+				
 				System.out.print("제목 : ");
 				String title = sc.nextLine();
 				System.out.print("내용 : ");
 				String body = sc.nextLine();
-
-				System.out.printf("%d번 글이 생성되었습니다.\n", id);
+				
+				articles.add(new Article(id, title, body));
+				
 				id++;
+				System.out.printf("%d번 글이 생성되었습니다.\n", id);
 			} else if (input.equals("exit")) {
 				System.out.println("== 프로그램 종료 ==");
 				break;
@@ -33,4 +47,22 @@ public class Main {
 		}
 	}
 
+}
+
+class Article {
+	int id;
+	String title;
+	String body;
+	
+	public Article(int id, String title, String body) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.body = body;
+	}
+	
+	@Override
+	public String toString() {
+		return title +"  /  "+ body ;
+	}
 }
